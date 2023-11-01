9e40624ae51519898128d4dfa19e3482e3283265
diorm47 <dior47m@gmail.com>
2023-11-01 22:13:30 +0500

    useful page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,6 +25,35 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $(".video_block_cards").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    speed: 1000,
+    prevArrow: $(".video_left"),
+    nextArrow: $(".video_right"),
+    responsive: [
+      {
+        breakpoint: 1110,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+
+
 // Tabs
 const { Tablist } = jolty;
 Tablist.initAll();
